3b6447e1a456c07bae16e7b9ab15d208a0daa197
Gaspar <gaspar.herman95@gmail.com>
2020-03-30 20:10:02 +0200

    Agregado Docker de compilación basado en el debian de laboratorio para compilar y ejecutar. - Modificado gitignore para evitar que se sigan los .projects del eclipse. - Agregada funcionalidad que permite leer el archivo .ttorrent correctamente

--- trivial_torrent-v1/trivial_torrent/src/ttorrent.c
+++ trivial_torrent-v1/trivial_torrent/src/ttorrent.c
@@ -5,10 +5,18 @@
 
 #include "file_io.h"
 #include "logger.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 // TODO: hey!? what is this?
 
 // https://en.wikipedia.org/wiki/Magic_number_(programming)#In_protocols
+
+void createServer(char *portCandidate);
+void createClient(char *metaFileToDownloadFromServer);
+
 static const uint32_t MAGIC_NUMBER = 0xde1c3230;
 
 static const uint8_t MSG_REQUEST = 0;
@@ -17,6 +25,59 @@ static const uint8_t MSG_RESPONSE_NA = 2;
 
 enum { RAW_MESSAGE_SIZE = 13 };
 
+void createServer(char *portCandidate ) {
+	int port = atoi(portCandidate);
+
+	if (port == 0) {
+		log_printf(LOG_INFO,"error: the %s is not a valid port\n",portCandidate);
+		exit(EXIT_FAILURE);
+	}
+
+	else if (port >= 1 && port <= 1024) {
+		log_printf(LOG_INFO, "error: the port %d is a reserved port\n",port);
+		exit(EXIT_FAILURE);
+	}
+
+	log_printf(LOG_INFO,"Server mode (port %d)\n",port);
+
+	//TODO: server stuff
+
+
+}
+
+void createClient(char *metaFileToDownloadFromServer){
+	char * extension = strtok(metaFileToDownloadFromServer,".");
+	char leftPartOfName[1024] = {0};
+	strcpy(leftPartOfName,extension);
+	strcat(leftPartOfName,".txt");
+	extension = strtok(NULL,".");
+	metaFileToDownloadFromServer = strcat(metaFileToDownloadFromServer,".ttorrent");
+
+
+	printf("parte izquierda %s\n",leftPartOfName);
+
+	if (extension == NULL) {
+		log_printf(LOG_INFO,"ERROR: NO EXTENSION FOUND");
+		exit(EXIT_FAILURE);
+	}
+
+	else if (strcmp(extension,"ttorrent") != 0){
+		log_printf(LOG_INFO,"ERROR: Invalid torrent file extension");
+		exit(EXIT_FAILURE);
+	}
+
+	struct torrent_t metaFileInfo;
+
+	int creationSuccess = create_torrent_from_metainfo_file(metaFileToDownloadFromServer,&metaFileInfo,leftPartOfName);
+
+	if (creationSuccess == -1 ){
+		log_printf(LOG_INFO, "error parsing file");
+	}
+
+
+
+
+}
 
 /**
  * Main function.
@@ -26,6 +87,32 @@ int main(int argc, char **argv) {
 	set_log_level(LOG_DEBUG);
 
 	log_printf(LOG_INFO, "Trivial Torrent (build %s %s) by %s", __DATE__, __TIME__, "J. DOE and J. DOE");
+
+	if (argc != 4 && argc != 2){
+		log_printf(LOG_INFO,"ERROR: Usage is: ttorrent [-l port] file.ttorrent\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	else if (argc == 4){
+		int foundServerPortFlag = 0;
+		int i;
+		for (i = 1; i < argc;i++){
+
+			if (strcmp("-l",argv[i]) == 0){
+				foundServerPortFlag = 1;
+				break;
+			}
+		}
+
+		if (foundServerPortFlag == 1) {
+			createServer(argv[i + 1]);
+		}
+	}
+
+	else {
+		createClient(argv[argc - 1]);
+	}
 
 	// ==========================================================================
 	// Parse command line
---
+++ ubuntuCompilator/readme.md
@@ -0,0 +1,3 @@
+### Docker machine for compile 
+
+-  Ubuntu 16.04's (one of setup develop) gcc compiler default version is 5.3. Even after update is uncapable of compile this project. Even a versión of 6.x is necessary. Thats the reasong behind this docker.
